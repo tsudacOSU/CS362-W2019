6370ff0f930a613a584fe488dc177dbf8d5685cf
Carly Tsuda <tsudac@flip3.engr.oregonstate.edu>
2019-02-10 15:04:29 -0800

    Finalized testme program for random testing quiz

--- projects/tsudac/quiz/testme.c
+++ projects/tsudac/quiz/testme.c
@@ -16,28 +16,28 @@ char inputChar()
 {
     char c;
 // will produce a random input from the ASCII table (1-33, 162 excluded)
-    c = (rand() % 129) + 33;
+    c = (rand() % 129) + 32;
     return c;
 }
 
 char *inputString()
 {
-    // 1/CHANCE chance that the test will produce a string that contains "reset", triggering one of the test cases
+    // 1/CHANCE chance that the test will produce a string that contains "reset", triggering the final test case
     // otherwise, generates a random string of size max string
-   char *string[MAX_STRING];
+   char string[MAX_STRING];
    int testCase = (rand() % CHANCE);
    if(testCase == 0)
-       string="reset";
+       strcpy(string, "reset");
    else
    {
         for(int i = 0; i < MAX_STRING; i++)
         {
-            string[i] = (rand() % 126) + 33;
+            string[i] = (rand() % 126) + 33; //this particular formula gives some unprintable characters; it does not seem to affect testing but looks kind of gross
         }
    }
    
    
-    return "";
+    return string;
 }
 
 void testme()
@@ -77,7 +77,6 @@ void testme()
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
-   
 
    /**********************************
     * for testing 
@@ -88,5 +87,5 @@ int main(int argc, char *argv[])
     **********************************/
 
     testme();
-    return 0;
+    return 0; //if this return statement is commented out, the gcov test reports 100% coverage.
 }
